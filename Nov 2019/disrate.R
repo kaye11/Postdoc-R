ce26f342d13dc68582ddaac19330381ad91801f9
kaye11 <karen.bondoc@gmail.com>
2021-05-06 22:55:23 -0400

    may2021_batch1

--- Nov 2019/disrate.R
+++ Nov 2019/disrate.R
@@ -2,12 +2,12 @@
 
 source("inspack.R")
 
-turb <- expand.grid(list (depth = (seq(-60, 0, 0.5)), wind = (seq(5, 35, 5))))
+turb <- expand.grid(list (depth = (seq(0, 50, 0.25)), wind = (seq(10, 50, 10))))
 
 #constants
 K= 0.4 #von Karman's constant
 Cd <- 1.15*(10)^-3 #drag coefficient
-Den_air <-  (1.225)*10^-3 #g/m3 at sea level and 15C
+Den_air <-  (1.212)*10^-3 #g/m3 at sea level and 18C #g/m3 at sea level and 15C
 Den_CH2O= 1.025 #g/m3 density seawater at 18C #these two will even out so no need to convert
 
 turb$U <- sqrt(Cd*((turb$wind)^2)*((Den_air/Den_CH2O)))
@@ -15,16 +15,18 @@ turb$disrate <- (turb$U^3)/(K*(abs(turb$depth)))
 #turb$U2 <- sqrt(turb$wind/(Den_CH2O*1000000))
 #turb$E2 <- (turb$U2^3)/(K*(abs(turb$depth)))
 
+BlRed <- colorRampPalette((c('#00429d', '#2b57a7', '#426cb0', '#5681b9', '#6997c2', '#7daeca', '#93c4d2', '#abdad9', '#caefdf', '#ffe2ca', '#ffc4b4', '#ffa59e', '#f98689', '#ed6976', '#dd4c65', '#ca2f55', '#b11346', '#93003a')))
+
 resize.win (6,6)
-ggplot(turb, aes(x=disrate, y=depth, color=as.factor(wind))) + geom_line(size=1) + labs (x=expression("dissipation rate"~ (m^2~s^-3)), y="depth (m)") + 
-  scale_x_log10(
-  breaks = scales::trans_breaks("log10", function(x) 10^x, n=6),
+ggplot(turb, aes(x=disrate, y=depth, color=as.factor(wind))) + geom_line(size=1) + labs (x=expression("dissipation rate"~ (m^2~s^-3)), y="depth (m)") + scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x, n=6),
   labels = scales::trans_format("log10", scales::math_format(10^.x))) + annotation_logticks(sides="b") +
-  theme_Publication2() + theme (legend.direction = "vertical", legend.position = c(0.80, 0.20), legend.key.width = unit(0.7, "cm")) + labs(color=expression("wind speed"~(m~s^-1))) + geom_hline(yintercept = (-30), linetype="dotted")
+  theme_Publication2() + theme (legend.direction = "vertical", legend.position = c(0.82, 0.25), legend.key.width = unit(0.7, "cm")) + labs(color=expression("wind speed"~(m~s^-1))) + geom_hline(yintercept =30, linetype="dotted")
 
-ggplot(turb, aes(x=log10(disrate), y=depth, color=as.factor(wind))) + geom_line(size=1) + labs (x=expression("log10 dissipation rate"~ (m^2~s^-3)), y="depth (m)") +
-  theme_Publication() + theme (legend.direction = "vertical", legend.position = c(0.80, 0.25), legend.key.width = unit(0.7, "cm")) + labs(color=expression("wind speed"~(m~s^-1))) + geom_hline(yintercept = (-30), linetype="dotted")
+resize.win (8,5)
+ggplot(turb, aes(x=log10(disrate), y=depth, color=as.factor(wind))) + geom_line(size=1) + 
+  labs (x=expression(log[10]~epsilon~(m^2~s^-3)),  y="depth (m)", 
+        color=expression("wind speed"~(m~s^-1))) + 
+  scale_x_continuous(breaks = c(seq(-8, -2, 1)), position="top") + 
+  scale_y_continuous(trans="reverse") +
+  theme_Publication() + theme (legend.direction = "vertical", legend.position = "right", legend.key.width = unit(0.7, "cm")) + geom_hline(yintercept = 30, linetype="dotted") + scale_color_manual (values= c ("#00429d",'#5681b9', '#ffc4b4','#ed6976', '#93003a' ))
 
-
-ggplot(turb, aes(x=log10(disrate), y=depth, color=as.factor(wind))) + geom_line(size=1) + labs (x=expression("log10 dissipation rate"~ (m^2~s^-3)), y="depth (m)") +
-  theme_Publication() + theme (legend.direction = "vertical", legend.position = c(0.80, 0.25), legend.key.width = unit(0.7, "cm")) + labs(color=expression("wind speed"~(m~s^-1))) + geom_hline(yintercept = (-30), linetype="dotted") + scale_y_continuous(position = "top")                                                                                                                                                                         
--- Nov 2019/wind_navice2.R
+++ Nov 2019/wind_navice2.R
@@ -1,5 +1,4 @@
-library(dplyr)
-library(plyr)
+source("inspack.R")
 
 multmerge = function(mypath){
   filenames=list.files(path=mypath, full.names=TRUE)
@@ -10,11 +9,13 @@ mydata=multmerge("d:/Postdoc/theoretical/NA-VICE/wind")
 
 mergeddata <- ldply(mydata, data.frame)
 
+#alidata <-  mergeddata %>% select (lon, lat, date, ISO_DateTime_UTC, wind_speed_c)
+#write.table (alidata, "Postdoc-R/Exported Tables/foralidata.csv", sep=";", col.names=T, row.names=F)
+
 ##just get date, wind_speed_c
-source("inspack.R")
 winddata <- mergeddata %>% select (date, wind_speed_c, wind_speed_r_port,	wind_speed_r_stbd,	wind_speed_c_port,	wind_speed_c_stbd)
 
-write.table (winddata, "Postdoc-R/Exported Tables/winddata.csv", sep=";", col.names=T, row.names=F)
+write.table (winddata, "Postdoc-R/Exported Tables/winddata_mar2021.csv", sep=";", col.names=T, row.names=F)
 winddata.raw.sum <- summarySE (winddata, measurevar = "wind_speed_c", groupvars = c ("date"))
 
 #get stations, cast, through date
@@ -22,7 +23,6 @@ casts <- read.csv("D:/R program/Postdoc-R/CSV Files/casts.csv")
 
 windmas <- left_join(winddata, casts, by = "date")
 winddata.sum <- summarySE (windmas, measurevar = "wind_speed_c", groupvars = c ("date", "cast", "Station", "Infection"))
-winddata.sum2 <- winddata.sum
 
 #use all data
 windmas <- windmas[rep(seq_len(nrow(windmas)), 301), ]
@@ -31,15 +31,15 @@ windmas$depth <- rep(c(seq(-150, 0, 0.5)), each=31680)
 #constants
 K= 0.4 #von Karman's constant
 Cd <- 1.15*(10)^-3 #drag coefficient
-Den_air <-  (1.225)*10^-3 #g/m3 at sea level and 15C
+Den_air <-  (1.212)*10^-3 #g/m3 at sea level and 18C
 Den_CH2O= 1.025 #g/m3 density seawater at 18C #these two will even out so no need to convert
+
+#use the means
+winddata.sum <- winddata.sum[rep(seq_len(nrow(winddata.sum)), 301), ]
+winddata.sum$depth <- rep(c(seq(-150, 0, 0.5)), each=21) #problem here
 windmas$U <- sqrt(Cd*((windmas$wind_speed_c)^2)*((Den_air/Den_CH2O)))
 windmas$Uc <- ((15.24/10)^0.143)*windmas$U
 windmas$disrate <- (windmas$U^3)/(K*(abs(windmas$depth)))
-
-#this plot is chaotic, dont run. using all data is such a bad idea
-#ggplot(windmas, aes(x=log10(E), y=depth, color=as.factor(cast))) + geom_smooth() + theme_clean() + labs (x="Dissipation rate", y="Depth")
-
 #use the means
 winddata.sum <- winddata.sum2
 winddata.sum <- winddata.sum[rep(seq_len(nrow(winddata.sum)), 301), ]
@@ -63,15 +63,16 @@ ggplot(winddata.sum %>% filter (depth>=-30), aes(x=log10(disrate), color=as.fact
 
 ggplot(winddata.sum %>% filter (disrate>1e-7) %>% filter (depth>=-30))+ geom_density(aes(x=log10(disrate), fill=Infection, color=Infection)) + facet_grid (Infection~.)+theme_Publication2() + labs (x="log10 dissipation rate") + theme (legend.position = "none")
 
+##wind data
 
 #save summary
 write.table(winddata.sum2, "Postdoc-R/Exported Tables/winddata_sum2.csv", sep=";", col.names=T, row.names=F)
 
-
 ##join wind data
+library(readxl)
 navice <- read_excel("D:/Postdoc/theoretical/NA-VICE/NAVICE Ehux Sytox VLP wrangled_KB_191119.xlsx")
 navice$date <- as.Date (navice$date)
 winddata.sum$date <- as.Date(winddata.sum$date)
 navice_alldata <- left_join(navice, winddata.sum %>% select (date, cast, Station, Infection, depth, disrate))
 
-write.table(navice_alldata, "Postdoc-R/Exported Tables/navice_alldata.csv", sep=";", col.names=T, row.names=F)
+write.table(navice_alldata, "Postdoc-R/Exported Tables/navice_alldata_18C.csv", sep=";", col.names=T, row.names=F)
